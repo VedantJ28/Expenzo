7639c09f530359bdf0d54d6af6362a99dd459fbc
Vedant Jadhav <vedantjadhav324@gmail.com>
2024-07-03 12:45:03 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const {db} = require("./DB/db")
+const { readdirSync } = require('fs');
 
 require("dotenv").config();
 
@@ -12,9 +13,9 @@ const PORT = process.env.PORT
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req,res)=>{
-    res.send("hello world!");
-})
+//routes
+readdirSync('./routes').map((route) => app.use('/api' , require('./routes/' + route)));
+
 
 const server = () =>{
     db();
